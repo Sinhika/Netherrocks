cdadcc690b91136f1532d72c5ab4317f8f4ca266
Sinhika <sinhika@republicofnewhome.org>
2018-09-21 07:44:38 -0500

    added root advancement, fyrite armor recipes

--- src/main/java/alexndr/plugins/netherrocks/Recipes.java
+++ src/main/java/alexndr/plugins/netherrocks/Recipes.java
@@ -8,19 +8,6 @@ public class Recipes
 //	public static void doRecipes()
 //	{
 //		// Armor Recipes
-//		// Fyrite Armor Recipes
-//		GameRegistry.addRecipe(new ItemStack(Content.fyrite_helmet, 1),
-//				new Object[] { "XXX", "X X", Character.valueOf('X'),
-//						Content.fyrite_ingot });
-//		GameRegistry.addRecipe(new ItemStack(Content.fyrite_chestplate, 1),
-//				new Object[] { "X X", "XXX", "XXX", Character.valueOf('X'),
-//						Content.fyrite_ingot });
-//		GameRegistry.addRecipe(new ItemStack(Content.fyrite_leggings, 1),
-//				new Object[] { "XXX", "X X", "X X", Character.valueOf('X'),
-//						Content.fyrite_ingot });
-//		GameRegistry.addRecipe(new ItemStack(Content.fyrite_boots, 1),
-//				new Object[] { "X X", "X X", Character.valueOf('X'),
-//						Content.fyrite_ingot });
 //					
 //		// Malachite Armor Recipes
 //		GameRegistry.addRecipe(new ItemStack(Content.malachite_helmet, 1),
