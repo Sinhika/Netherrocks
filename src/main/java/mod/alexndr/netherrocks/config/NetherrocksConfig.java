b246cd9d0fb87366a80bd409640e82c58f74b532
Cynthia Higginbotham <cchjob@republicofnewhome.org>
2019-06-30 08:45:40 -0500

    netherrocks ore generation drafted

--- src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
+++ src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
@@ -3,5 +3,9 @@ package mod.alexndr.netherrocks.config;
 public class NetherrocksConfig
 {
     public static boolean enableArgoniteOre  = true;
-    // TODO
+    public static boolean enableAshstoneOre  = true;
+    public static boolean enableDragonstoneOre  = true;
+    public static boolean enableFyriteOre  = true;
+    public static boolean enableIllumeniteOre  = true;
+    public static boolean enableMalachiteOre  = true;
 }  // end class NetherrocksConfig
--- src/main/java/mod/alexndr/netherrocks/generation/OreGeneration.java
+++ src/main/java/mod/alexndr/netherrocks/generation/OreGeneration.java
@@ -2,11 +2,13 @@ package mod.alexndr.netherrocks.generation;
 
 import mod.alexndr.netherrocks.config.NetherrocksConfig;
 import mod.alexndr.netherrocks.init.ModBlocks;
+import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.ReplaceBlockConfig;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
 
@@ -17,9 +19,19 @@ public class OreGeneration
 {
     // Vein/Chunk Count, MinHeight, MaxHeightBase, MaxHeight
     // TODO
-    private static final CountRangeConfig argonite_cfg = new CountRangeConfig(5, 10, 0, 128);
-    private static final int argonite_veinsize = 7;
+    private static final CountRangeConfig argonite_cfg = new CountRangeConfig(10, 0, 0, 128);
+    private static final int argonite_veinsize = 6;
+    private static final CountRangeConfig ashstone_cfg = new CountRangeConfig(10, 0, 0, 128);
+    private static final int ashstone_veinsize = 5;
+    private static final CountRangeConfig dragonstone_cfg = new CountRangeConfig(6, 0, 0, 128);
+    private static final int dragonstone_veinsize = 5;
+    private static final CountRangeConfig fyrite_cfg = new CountRangeConfig(10, 0, 0, 128);
+    private static final int fyrite_veinsize = 6;
+    private static final CountRangeConfig malachite_cfg = new CountRangeConfig(10, 0, 0, 128);
+    private static final int malachite_veinsize = 7;
 
+    private static final CountRangeConfig illumenite_cfg = new CountRangeConfig(350, 1, 0, 128);
+    private static final int illumenite_veinsize = 15;
     /**
      * called in setup to generate Nether ores.
      */
@@ -33,7 +45,50 @@ public class OreGeneration
                                                  ModBlocks.argonite_ore.getDefaultState(), argonite_veinsize),
                                      Placement.COUNT_RANGE, argonite_cfg));
         }
-        // TODO
+        if (NetherrocksConfig.enableAshstoneOre)
+        {
+            Biomes.NETHER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                 Biome.createDecoratedFeature(Feature.ORE,
+                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                                             ModBlocks.ashstone_ore.getDefaultState(), ashstone_veinsize),
+                                             Placement.COUNT_RANGE, ashstone_cfg));
+
+        }
+        if (NetherrocksConfig.enableDragonstoneOre)
+        {
+            Biomes.NETHER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                Biome.createDecoratedFeature(Feature.ORE,
+                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                                             ModBlocks.dragonstone_ore.getDefaultState(), dragonstone_veinsize),
+                                             Placement.COUNT_RANGE, dragonstone_cfg));
+
+        }
+        if (NetherrocksConfig.enableFyriteOre)
+        {
+            Biomes.NETHER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                Biome.createDecoratedFeature(Feature.ORE,
+                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                                             ModBlocks.fyrite_ore.getDefaultState(), fyrite_veinsize),
+                                             Placement.COUNT_RANGE, fyrite_cfg));
+
+        }
+        if (NetherrocksConfig.enableMalachiteOre)
+        {
+            Biomes.NETHER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                Biome.createDecoratedFeature(Feature.ORE,
+                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK,
+                                             ModBlocks.malachite_ore.getDefaultState(), malachite_veinsize),
+                                             Placement.COUNT_RANGE, malachite_cfg));
+
+        }
+        if (NetherrocksConfig.enableIllumeniteOre)
+        {
+            // TODO - this will need tweaking if it works at all.
+            Biomes.NETHER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
+                Biome.createDecoratedFeature(Feature.EMERALD_ORE,
+                        new ReplaceBlockConfig(Blocks.GLOWSTONE.getDefaultState(), ModBlocks.illumenite_ore.getDefaultState()),
+                                             Placement.COUNT_RANGE, illumenite_cfg));
+        }
     } // end setupNetherOreGen()
 
 }  // end class OreGeneration
