942aff5289c29630473803d0a24101ae944358be
Cynthia Higginbotham <cchjob@republicofnewhome.org>
2019-07-25 21:43:37 -0500

    updating to 1.14.4; also starting NetherFurnace object work.

---
+++ src/main/java/mod/alexndr/netherrocks/content/NetherFurnaceBlock.java
@@ -0,0 +1,19 @@
+package mod.alexndr.netherrocks.content;
+
+import net.minecraft.block.FurnaceBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+
+public class NetherFurnaceBlock extends FurnaceBlock
+{
+    public NetherFurnaceBlock(Properties builder)
+    {
+        super(builder);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(IBlockReader worldIn)
+    {
+        return super.createNewTileEntity(worldIn);
+    }
+}  // end class NetherFurnaceBlock
