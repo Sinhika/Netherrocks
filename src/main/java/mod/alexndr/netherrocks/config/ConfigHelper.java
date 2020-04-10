2d7862e4a6d927c5c2e6062fa8eef5631064a679
Sinhika <sinhika@republicofnewhome.org>
2020-04-10 14:50:26 -0500

    added in all ore-generation params

--- src/main/java/mod/alexndr/netherrocks/config/ConfigHelper.java
+++ src/main/java/mod/alexndr/netherrocks/config/ConfigHelper.java
@@ -1,5 +1,6 @@
 package mod.alexndr.netherrocks.config;
 
+import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraftforge.fml.config.ModConfig;
 
 /**
@@ -16,11 +17,52 @@ public final class ConfigHelper
 	public static void bakeServer(final ModConfig config)
 	{
 		NetherrocksConfig.enableArgoniteOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
-		NetherrocksConfig.enableAshstoneOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
-		NetherrocksConfig.enableDragonstoneOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
-		NetherrocksConfig.enableFyriteOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
-		NetherrocksConfig.enableIllumeniteOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
-		NetherrocksConfig.enableMalachiteOre = ConfigHolder.SERVER.serverEnableArgoniteOre.get();
+        NetherrocksConfig.argonite_veinsize = ConfigHolder.SERVER.serverArgoniteVeinSize.get();
+        NetherrocksConfig.argonite_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverArgoniteVeinCount.get(),
+                ConfigHolder.SERVER.serverArgoniteBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverArgoniteMaxHeight.get() 
+        );
+        
+		NetherrocksConfig.enableAshstoneOre = ConfigHolder.SERVER.serverEnableAshstoneOre.get();
+        NetherrocksConfig.ashstone_veinsize = ConfigHolder.SERVER.serverAshstoneVeinSize.get();
+        NetherrocksConfig.ashstone_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverAshstoneVeinCount.get(),
+                ConfigHolder.SERVER.serverAshstoneBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverAshstoneMaxHeight.get() 
+        );
+        
+		NetherrocksConfig.enableDragonstoneOre = ConfigHolder.SERVER.serverEnableDragonstoneOre.get();
+        NetherrocksConfig.dragonstone_veinsize = ConfigHolder.SERVER.serverDragonstoneVeinSize.get();
+        NetherrocksConfig.dragonstone_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverDragonstoneVeinCount.get(),
+                ConfigHolder.SERVER.serverDragonstoneBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverDragonstoneMaxHeight.get() 
+        );
+        
+		NetherrocksConfig.enableFyriteOre = ConfigHolder.SERVER.serverEnableFyriteOre.get();
+        NetherrocksConfig.fyrite_veinsize = ConfigHolder.SERVER.serverFyriteVeinSize.get();
+        NetherrocksConfig.fyrite_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverFyriteVeinCount.get(),
+                ConfigHolder.SERVER.serverFyriteBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverFyriteMaxHeight.get() 
+        );
+
+        NetherrocksConfig.enableIllumeniteOre = ConfigHolder.SERVER.serverEnableIllumeniteOre.get();
+        NetherrocksConfig.illumenite_veinsize = ConfigHolder.SERVER.serverIllumeniteVeinSize.get();
+        NetherrocksConfig.illumenite_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverIllumeniteVeinCount.get(),
+                ConfigHolder.SERVER.serverIllumeniteBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverIllumeniteMaxHeight.get() 
+        );
+
+        NetherrocksConfig.enableMalachiteOre = ConfigHolder.SERVER.serverEnableMalachiteOre.get();
+        NetherrocksConfig.malachite_veinsize = ConfigHolder.SERVER.serverMalachiteVeinSize.get();
+        NetherrocksConfig.malachite_cfg = new CountRangeConfig(  
+                ConfigHolder.SERVER.serverMalachiteVeinCount.get(),
+                ConfigHolder.SERVER.serverMalachiteBottomHeight.get(), 
+                0, ConfigHolder.SERVER.serverMalachiteMaxHeight.get() 
+        );
 	} // end bakeServer()
 
 	@SuppressWarnings("unused")
--- src/main/java/mod/alexndr/netherrocks/config/ServerConfig.java
+++ src/main/java/mod/alexndr/netherrocks/config/ServerConfig.java
@@ -14,39 +14,184 @@ final class ServerConfig
 {
 
 	final ForgeConfigSpec.BooleanValue serverEnableArgoniteOre;
+    final ForgeConfigSpec.IntValue serverArgoniteVeinSize;
+    final ForgeConfigSpec.IntValue serverArgoniteVeinCount;
+    final ForgeConfigSpec.IntValue serverArgoniteBottomHeight;
+    final ForgeConfigSpec.IntValue serverArgoniteMaxHeight;
+	
 	final ForgeConfigSpec.BooleanValue serverEnableAshstoneOre;
-	final ForgeConfigSpec.BooleanValue serverEnableDragonstoneOre;
-	final ForgeConfigSpec.BooleanValue serverEnableFyriteOre;
-	final ForgeConfigSpec.BooleanValue serverEnableIllumeniteOre;
-	final ForgeConfigSpec.BooleanValue serverEnableMalachiteOre;
+    final ForgeConfigSpec.IntValue serverAshstoneVeinSize;
+    final ForgeConfigSpec.IntValue serverAshstoneVeinCount;
+    final ForgeConfigSpec.IntValue serverAshstoneBottomHeight;
+    final ForgeConfigSpec.IntValue serverAshstoneMaxHeight;
 
+    final ForgeConfigSpec.BooleanValue serverEnableDragonstoneOre;
+    final ForgeConfigSpec.IntValue serverDragonstoneVeinSize;
+    final ForgeConfigSpec.IntValue serverDragonstoneVeinCount;
+    final ForgeConfigSpec.IntValue serverDragonstoneBottomHeight;
+    final ForgeConfigSpec.IntValue serverDragonstoneMaxHeight;
+
+    final ForgeConfigSpec.BooleanValue serverEnableFyriteOre;
+    final ForgeConfigSpec.IntValue serverFyriteVeinSize;
+    final ForgeConfigSpec.IntValue serverFyriteVeinCount;
+    final ForgeConfigSpec.IntValue serverFyriteBottomHeight;
+    final ForgeConfigSpec.IntValue serverFyriteMaxHeight;
+
+    final ForgeConfigSpec.BooleanValue serverEnableIllumeniteOre;
+    final ForgeConfigSpec.IntValue serverIllumeniteVeinSize;
+    final ForgeConfigSpec.IntValue serverIllumeniteVeinCount;
+    final ForgeConfigSpec.IntValue serverIllumeniteBottomHeight;
+    final ForgeConfigSpec.IntValue serverIllumeniteMaxHeight;
+
+    final ForgeConfigSpec.BooleanValue serverEnableMalachiteOre;
+    final ForgeConfigSpec.IntValue serverMalachiteVeinSize;
+    final ForgeConfigSpec.IntValue serverMalachiteVeinCount;
+    final ForgeConfigSpec.IntValue serverMalachiteBottomHeight;
+    final ForgeConfigSpec.IntValue serverMalachiteMaxHeight;
+
+    
 	ServerConfig(final ForgeConfigSpec.Builder builder)
 	{
-		builder.push("general");
+		builder.push("OreGeneration");
 		serverEnableArgoniteOre = builder
 				.comment("Enable argonite ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableArgoniteOre")
-				.define("serverEnableArgoniteOre", true);
+				.define("EnableArgoniteOre", true);
 		serverEnableAshstoneOre = builder
 				.comment("Enable ashstone ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableAshstoneOre")
-				.define("serverEnableAshstoneOre", true);
+				.define("EnableAshstoneOre", true);
 		serverEnableDragonstoneOre = builder
 				.comment("Enable dragonstone ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableDragonstoneOre")
-				.define("serverEnableDragonstoneOre", true);
+				.define("EnableDragonstoneOre", true);
 		serverEnableFyriteOre = builder
 				.comment("Enable fyrite ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableFyriteOre")
-				.define("serverEnableFyriteOre", true);
+				.define("EnableFyriteOre", true);
 		serverEnableIllumeniteOre = builder
 				.comment("Enable illumenite ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableIllumeniteOre")
-				.define("serverEnableIllumeniteOre", true);
+				.define("EnableIllumeniteOre", true);
 		serverEnableMalachiteOre = builder
 				.comment("Enable malachite ore generation?")
 				.translation(Netherrocks.MODID + ".config.serverEnableMalachiteOre")
-				.define("serverEnableMalachiteOre", true);
+				.define("EnableMalachiteOre", true);
+		
+        builder.push("Argonite");
+        serverArgoniteVeinSize = builder
+                .comment("Argonite ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverArgoniteVeinSize")
+                .defineInRange("ArgoniteVeinSize", 6, 1, Integer.MAX_VALUE);
+        serverArgoniteVeinCount = builder
+                .comment("Argonite ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverArgoniteVeinCount")
+                .defineInRange("ArgoniteVeinCount", 10, 1, Integer.MAX_VALUE);
+        serverArgoniteBottomHeight = builder
+                .comment("Argonite ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverArgoniteBottomHeight")
+                .defineInRange("ArgoniteBottomHeight", 0, 1, 128);
+        serverArgoniteMaxHeight = builder
+                .comment("Argonite ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverArgoniteMaxHeight")
+                .defineInRange("ArgoniteMaxHeight", 128, 1, 128);
+        builder.pop();
+               
+        builder.push("Ashstone");
+        serverAshstoneVeinSize = builder
+                .comment("Ashstone ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverAshstoneVeinSize")
+                .defineInRange("AshstoneVeinSize", 5, 1, Integer.MAX_VALUE);
+        serverAshstoneVeinCount = builder
+                .comment("Ashstone ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverAshstoneVeinCount")
+                .defineInRange("AshstoneVeinCount", 10, 1, Integer.MAX_VALUE);
+        serverAshstoneBottomHeight = builder
+                .comment("Ashstone ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverAshstoneBottomHeight")
+                .defineInRange("AshstoneBottomHeight", 0, 1, 127);
+        serverAshstoneMaxHeight = builder
+                .comment("Ashstone ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverAshstoneMaxHeight")
+                .defineInRange("AshstoneMaxHeight", 128, 1, 128);
+        builder.pop();
+        
+        builder.push("Dragonstone");
+        serverDragonstoneVeinSize = builder
+                .comment("Dragonstone ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverDragonstoneVeinSize")
+                .defineInRange("DragonstoneVeinSize", 5, 1, Integer.MAX_VALUE);
+        serverDragonstoneVeinCount = builder
+                .comment("Dragonstone ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverDragonstoneVeinCount")
+                .defineInRange("DragonstoneVeinCount", 7, 1, Integer.MAX_VALUE);
+        serverDragonstoneBottomHeight = builder
+                .comment("Dragonstone ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverDragonstoneBottomHeight")
+                .defineInRange("DragonstoneBottomHeight", 0, 1, 127);
+        serverDragonstoneMaxHeight = builder
+                .comment("Dragonstone ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverDragonstoneMaxHeight")
+                .defineInRange("DragonstoneMaxHeight", 128, 1, 128);
+        builder.pop();
+        
+        builder.push("Fyrite");
+        serverFyriteVeinSize = builder
+                .comment("Fyrite ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverFyriteVeinSize")
+                .defineInRange("FyriteVeinSize", 6, 1, Integer.MAX_VALUE);
+        serverFyriteVeinCount = builder
+                .comment("Fyrite ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverFyriteVeinCount")
+                .defineInRange("FyriteVeinCount", 10, 1, Integer.MAX_VALUE);
+        serverFyriteBottomHeight = builder
+                .comment("Fyrite ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverFyriteBottomHeight")
+                .defineInRange("FyriteBottomHeight", 0, 1, 127);
+        serverFyriteMaxHeight = builder
+                .comment("Fyrite ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverFyriteMaxHeight")
+                .defineInRange("FyriteMaxHeight", 128, 1, 128);
+        builder.pop();
+        
+        builder.push("Illumenite");
+        serverIllumeniteVeinSize = builder
+                .comment("Illumenite ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverIllumeniteVeinSize")
+                .defineInRange("IllumeniteVeinSize", 15, 1, Integer.MAX_VALUE);
+        serverIllumeniteVeinCount = builder
+                .comment("Illumenite ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverIllumeniteVeinCount")
+                .defineInRange("IllumeniteVeinCount", 5, 1, Integer.MAX_VALUE);
+        serverIllumeniteBottomHeight = builder
+                .comment("Illumenite ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverIllumeniteBottomHeight")
+                .defineInRange("IllumeniteBottomHeight", 0, 1, 127);
+        serverIllumeniteMaxHeight = builder
+                .comment("Illumenite ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverIllumeniteMaxHeight")
+                .defineInRange("IllumeniteMaxHeight", 128, 1, 128);
+        builder.pop();
+        
+        builder.push("Malachite");
+        serverMalachiteVeinSize = builder
+                .comment("Malachite ore vein size")
+                .translation(Netherrocks.MODID + ".config.serverMalachiteVeinSize")
+                .defineInRange("MalachiteVeinSize", 7, 1, Integer.MAX_VALUE);
+        serverMalachiteVeinCount = builder
+                .comment("Malachite ore vein count per chunk")
+                .translation(Netherrocks.MODID + ".config.serverMalachiteVeinCount")
+                .defineInRange("MalachiteVeinCount", 10, 1, Integer.MAX_VALUE);
+        serverMalachiteBottomHeight = builder
+                .comment("Malachite ore minimum height")
+                .translation(Netherrocks.MODID + ".config.serverMalachiteBottomHeight")
+                .defineInRange("MalachiteBottomHeight", 0, 0, 127);
+        serverMalachiteMaxHeight = builder
+                .comment("Malachite ore maximum height")
+                .translation(Netherrocks.MODID + ".config.serverMalachiteMaxHeight")
+                .defineInRange("MalachiteMaxHeight", 128, 1, 128);
+        builder.pop();
+        
 		builder.pop();
 	} // end ctor
 
