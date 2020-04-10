66fff7ca6da603434cb3de6513026376c8340f52
Sinhika <sinhika@republicofnewhome.org>
2020-04-10 14:26:57 -0500

    fixed nether furnace block to (a) have a texture, (b) show sparks, and (c) light the area

--- src/main/java/mod/alexndr/netherrocks/content/NetherFurnaceBlock.java
+++ src/main/java/mod/alexndr/netherrocks/content/NetherFurnaceBlock.java
@@ -1,5 +1,7 @@
 package mod.alexndr.netherrocks.content;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import mod.alexndr.netherrocks.init.ModTiles;
@@ -10,6 +12,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
@@ -18,10 +21,14 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
@@ -153,4 +160,32 @@ public class NetherFurnaceBlock extends HorizontalBlock
        return state.rotate(mirrorIn.toRotation(state.get(HORIZONTAL_FACING)));
    }
 
+   /**
+    * Called periodically clientside on blocks near the player to show effects (like furnace fire particles). Note that
+    * this method is unrelated to {@link randomTick} and {@link #needsRandomTick}, and will always be called regardless
+    * of whether the block can receive random update ticks
+    */
+   @OnlyIn(Dist.CLIENT)
+   @Override
+   public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+      if (stateIn.get(BURNING)) {
+         double d0 = (double)pos.getX() + 0.5D;
+         double d1 = (double)pos.getY();
+         double d2 = (double)pos.getZ() + 0.5D;
+         if (rand.nextDouble() < 0.1D) {
+            worldIn.playSound(d0, d1, d2, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+         }
+
+         Direction direction = stateIn.get(HORIZONTAL_FACING);
+         Direction.Axis direction$axis = direction.getAxis();
+//         double d3 = 0.52D;
+         double d4 = rand.nextDouble() * 0.6D - 0.3D;
+         double d5 = direction$axis == Direction.Axis.X ? (double)direction.getXOffset() * 0.52D : d4;
+         double d6 = rand.nextDouble() * 6.0D / 16.0D;
+         double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getZOffset() * 0.52D : d4;
+         worldIn.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+         worldIn.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+      }
+   } // end animateTick()
+   
 }  // end class NetherFurnaceBlock
