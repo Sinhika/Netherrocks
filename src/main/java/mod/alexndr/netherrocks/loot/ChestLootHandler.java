f71a5c7e6ec7c65ca6a181ba898e7435b6a321ba
Sinhika <sinhika@republicofnewhome.org>
2020-06-27 15:06:59 -0500

    added ChestLootHandler to netherrocks
    (cherry picked from commit 532ccabea3620ae1403707c4e830e59e82865150)

---
+++ src/main/java/mod/alexndr/netherrocks/loot/ChestLootHandler.java
@@ -0,0 +1,31 @@
+package mod.alexndr.netherrocks.loot;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.TableLootEntry;
+
+/**
+ * Code that handles injecting seeds into chest loot pools.
+ * Based heavily on Botania's LootHandler, without which I never would have
+ * figured this out.
+ * 
+ */
+public final class ChestLootHandler
+{
+    // lets figure out how to write a LootPool
+    public static LootPool getInjectPool(String modid, String entryName) 
+    {
+        return LootPool.builder()
+                .addEntry(getInjectEntry(modid, entryName, 1))
+                .bonusRolls(0, 1)
+                .name(modid + "_inject")
+                .build();
+    }
+
+    private static LootEntry.Builder<?> getInjectEntry(String modid, String name, int weight) {
+        ResourceLocation table = new ResourceLocation(modid, "inject/" + name);
+        return TableLootEntry.builder(table).weight(weight);
+    }
+
+} // end-class
