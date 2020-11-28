f1c55da569d5a953aea6e697b3801f28395f7dd0
Sinhika <sinhika@republicofnewhome.org>
2020-11-28 11:16:31 -0600

    made NetherrocksConfig a proper ISimpleConfig object

--- src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
+++ src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
@@ -1,9 +1,15 @@
 package mod.alexndr.netherrocks.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import mod.alexndr.simplecorelib.config.ISimpleConfig;
 import mod.alexndr.simplecorelib.config.ModOreConfig;
 
-public class NetherrocksConfig
+public class NetherrocksConfig  implements ISimpleConfig
 {
+    private static Map<String, Boolean> flags = new HashMap<>();
+
     public static final int illumeniteNVTime = 3600;        // night vision time (seconds)
     public static final int illumeniteBlindnessTime = 60;   // target blindness time (seconds)
     public static final int illumeniteSlowTime = 200;       // target slow time (seconds)
@@ -26,4 +32,24 @@ public class NetherrocksConfig
     public static ModOreConfig malachite_cfg;
     public static ModOreConfig illumenite_cfg;
 
+    public static NetherrocksConfig INSTANCE = new NetherrocksConfig();
+
+    @Override
+    public void clear()
+    {
+        flags.clear();
+    }
+
+    @Override
+    public boolean getFlag(String n)
+    {
+        Boolean obj = flags.get(n);
+        return obj != null && obj;
+    }
+
+    @Override
+    public void putFlag(String n, boolean val)
+    {
+        flags.put(n, val);
+    }
 }  // end class NetherrocksConfig
