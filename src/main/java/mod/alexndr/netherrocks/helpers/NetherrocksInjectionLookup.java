a5f18b302cba439c07f56515f3e27e5be60f9b6f
Sinhika <sinhika@republicofnewhome.org>
2021-04-24 15:34:46 -0500

    overhauled NetherrocksInjectionLookup for easier maintenance

--- src/main/java/mod/alexndr/netherrocks/helpers/NetherrocksInjectionLookup.java
+++ src/main/java/mod/alexndr/netherrocks/helpers/NetherrocksInjectionLookup.java
@@ -9,34 +9,11 @@ public class NetherrocksInjectionLookup extends InjectionTableLookup
     public NetherrocksInjectionLookup()
     {
         super();
-        
-        // nether loot in the nether.
-        this.replace("nether_bridge", "bastion");
-        
-        // we don't have a 'village_house' table.
-        this.replace("village_savanna_house", null);
-        this.replace("village_plains_house", null);
-        this.replace("village_desert_house", null);
-        this.replace("village_snowy_house", null);
-        this.replace("village_taiga_house", null);
-
-        // some locations shouldn't have nether loot.
-        this.replace("shipwreck_supply", null);
-        this.replace("shipwreck_map", null);
-        this.replace("shipwreck_treasure", null);
-        this.replace("underwater_ruin_small", null);
-        this.replace("underwater_ruin_big", null);
-        this.replace("abandoned_mineshaft", null);
-        this.replace("igloo_chest", null);
-        this.replace("simple_dungeon", null);
-        this.replace("village_armorer", null);
-        this.replace("village_mason", null);
-        this.replace("village_toolsmith", null);
-        this.replace("village_weaponsmith", null);
-        
-        // these tables all present as themselves.
-        // nether loot near the nether; table present as itself.
-        this.remove("ruined_portal");
+        this.AddNetherAliases();
+        this.AddStrongholdAliases();
+        this.put("desert_pyramid", "desert_pyramid");
+        this.put("jungle_temple", "jungle_temple");
+        this.put("ruined_portal", "ruined_portal");
     } // end ctor
 
 } // end class
