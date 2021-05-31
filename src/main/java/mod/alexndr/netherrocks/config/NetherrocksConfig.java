c7033c180ca74267d017672cd0bc521594308dbe
Sinhika <sinhika@republicofnewhome.org>
2021-05-31 15:56:24 -0500

    fixed up config

--- src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
+++ src/main/java/mod/alexndr/netherrocks/config/NetherrocksConfig.java
@@ -1,20 +1,14 @@
 package mod.alexndr.netherrocks.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import mod.alexndr.simplecorelib.config.ISimpleConfig;
 import mod.alexndr.simplecorelib.config.ModOreConfig;
+import mod.alexndr.simplecorelib.config.SimpleConfig;
 
-public class NetherrocksConfig  implements ISimpleConfig
+public class NetherrocksConfig  extends SimpleConfig
 {
-    private static Map<String, Boolean> flags = new HashMap<>();
-
     public static final int illumeniteNVTime = 3600;        // night vision time (seconds)
     public static final int illumeniteBlindnessTime = 60;   // target blindness time (seconds)
     public static final int illumeniteSlowTime = 200;       // target slow time (seconds)
     public static final int illumeniteSlowLevel = 3;        // slow level
-    
     
     public static boolean enableArgoniteOre;
     public static boolean enableAshstoneOre;
@@ -34,22 +28,4 @@ public class NetherrocksConfig  implements ISimpleConfig
 
     public static NetherrocksConfig INSTANCE = new NetherrocksConfig();
 
-    @Override
-    public void clear()
-    {
-        flags.clear();
-    }
-
-    @Override
-    public boolean getFlag(String n)
-    {
-        Boolean obj = flags.get(n);
-        return obj != null && obj;
-    }
-
-    @Override
-    public void putFlag(String n, boolean val)
-    {
-        flags.put(n, val);
-    }
 }  // end class NetherrocksConfig
