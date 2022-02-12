765c4cc684972d459b583e9125f4d56947638322
Sinhika <sinhika@republicofnewhome.org>
2022-02-12 13:49:10 -0600

    added raw fyrite and raw fyrite block to fuels

--- src/main/java/mod/alexndr/netherrocks/api/content/AbstractNetherFurnaceTileEntity.java
+++ src/main/java/mod/alexndr/netherrocks/api/content/AbstractNetherFurnaceTileEntity.java
@@ -56,7 +56,9 @@ public abstract class AbstractNetherFurnaceTileEntity extends VeryAbstractFurnac
         {
             validFuels.add(Blocks.NETHERRACK.asItem());
             validFuels.add(ModBlocks.fyrite_block.get().asItem());
+            validFuels.add(ModBlocks.raw_fyrite_block.get().asItem());
             validFuels.add(ModItems.fyrite_ingot.get());
+            validFuels.add(ModItems.raw_fyrite.get());
             validFuels.add(ModItems.fyrite_nugget.get());
             validFuels.add(ModItems.fyrite_dust.get());
             validFuels.add(ModBlocks.fyrite_bricks.get().asItem());
@@ -78,11 +80,13 @@ public abstract class AbstractNetherFurnaceTileEntity extends VeryAbstractFurnac
         Map<Item, Integer> map = Maps.newLinkedHashMap();
         addItemBurnTime(map, Blocks.NETHERRACK, netherrackBurnTime);
         addItemBurnTime(map, ModBlocks.fyrite_block.get(), fyriteBurnTime * 10);
+        addItemBurnTime(map, ModBlocks.raw_fyrite_block.get(), fyriteBurnTime * 10);
         addItemBurnTime(map, ModBlocks.fyrite_bricks.get(), fyriteBurnTime * 10);
         addItemBurnTime(map, ModBlocks.fyrite_brick_stairs.get(), fyriteBurnTime * 10);
         addItemBurnTime(map, ModBlocks.fyrite_brick_slab.get(), fyriteBurnTime * 5);
         addItemBurnTime(map, ModBlocks.fyrite_door.get(), fyriteBurnTime * 5);
         addItemBurnTime(map, ModItems.fyrite_ingot.get(), fyriteBurnTime);
+        addItemBurnTime(map, ModItems.raw_fyrite.get(), fyriteBurnTime);
         addItemBurnTime(map, ModItems.fyrite_nugget.get(), fyriteBurnTime/9);
         addItemBurnTime(map, ModItems.fyrite_dust.get(), fyriteBurnTime/2);
         addItemBurnTime(map, Items.BLAZE_ROD, blazeRodBurnTime);
