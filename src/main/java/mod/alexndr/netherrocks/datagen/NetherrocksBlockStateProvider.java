dc872db509f0242e5513aa34a54b86e258a63dbb
Sinhika <sinhika@republicofnewhome.org>
2022-05-23 17:50:42 -0500

    moved argonite brick block models and blockstates to datagen. Implemented new 'cut_argonite' texture for argonite brick blocks

--- src/main/java/mod/alexndr/netherrocks/datagen/NetherrocksBlockStateProvider.java
+++ src/main/java/mod/alexndr/netherrocks/datagen/NetherrocksBlockStateProvider.java
@@ -45,15 +45,16 @@ public class NetherrocksBlockStateProvider extends SimpleBlockStateProvider
         ModelFile malachitePlateModel_down = this.models().pressurePlateDown("malachite_plate_down", 
                 new ResourceLocation(Netherrocks.MODID, "block/malachite_block"));
     
-        // ITEM MODELS
+        
+        // ITEM MODELS -- pressure plates
         this.itemModels().withExistingParent("argonite_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/argonite_plate"));
         this.itemModels().withExistingParent("ashstone_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/ashstone_plate"));
         this.itemModels().withExistingParent("dragonstone_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/dragonstone_plate"));
         this.itemModels().withExistingParent("fyrite_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/fyrite_plate"));
         this.itemModels().withExistingParent("illumenite_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/illumenite_plate"));
         this.itemModels().withExistingParent("malachite_pressure_plate", new ResourceLocation(Netherrocks.MODID, "block/malachite_plate"));
-       
-        // BLOCKSTATES
+         
+        // BLOCKSTATES -- pressure plates
         this.buildWeightedPressurePlateBlockState(ModBlocks.argonite_pressure_plate.get(), argonitePlateModel, argonitePlateModel_down);
         this.buildWeightedPressurePlateBlockState(ModBlocks.ashstone_pressure_plate.get(), ashstonePlateModel, ashstonePlateModel_down);
         this.buildWeightedPressurePlateBlockState(ModBlocks.dragonstone_pressure_plate.get(), dragonstonePlateModel, dragonstonePlateModel_down);
@@ -61,6 +62,22 @@ public class NetherrocksBlockStateProvider extends SimpleBlockStateProvider
         this.buildWeightedPressurePlateBlockState(ModBlocks.illumenite_pressure_plate.get(), illumenitePlateModel, illumenitePlateModel_down);
         this.buildWeightedPressurePlateBlockState(ModBlocks.malachite_pressure_plate.get(), malachitePlateModel, malachitePlateModel_down);
                
+        // BLOCKSTATES -- brick blocks, but have "cut_metal" texture names.
+        this.simpleBlock(ModBlocks.argonite_bricks.get(), 
+                this.models().cubeAll("argonite_bricks", new ResourceLocation(Netherrocks.MODID, "block/cut_argonite")));
+        
+        // BLOCKSTATES -- brick slabs, but have "cut_metal" texture names.
+        this.slabBlock(ModBlocks.argonite_brick_slab.get(), new ResourceLocation(Netherrocks.MODID, "block/argonite_bricks"), 
+                new ResourceLocation(Netherrocks.MODID, "block/cut_argonite"));
+        
+        // BLOCKSTATES -- brick stairs, but have "cut metal" textures.
+        this.stairsBlock(ModBlocks.argonite_brick_stairs.get(), new ResourceLocation(Netherrocks.MODID, "block/cut_argonite"));
+        
+        // ITEM MODELS -- brick blocks
+        this.itemModels().withExistingParent("argonite_bricks", new ResourceLocation(Netherrocks.MODID, "block/argonite_bricks"));
+        this.itemModels().withExistingParent("argonite_brick_slab", new ResourceLocation(Netherrocks.MODID, "block/argonite_brick_slab"));
+        this.itemModels().withExistingParent("argonite_brick_stairs", new ResourceLocation(Netherrocks.MODID, "block/argonite_brick_stairs"));
+        
     } // end registerStatesAndModels()
 
 } // end class
