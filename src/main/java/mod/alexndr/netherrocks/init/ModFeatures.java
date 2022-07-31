0f62caa4cf5b49d7971d84280b2330e091dde407
Sinhika <sinhika@republicofnewhome.org>
2022-07-31 11:04:41 -0500

    yes, we need IllumeniteBlobFeature class after all. Started creating configured_feature jsons

--- src/main/java/mod/alexndr/netherrocks/init/ModFeatures.java
+++ src/main/java/mod/alexndr/netherrocks/init/ModFeatures.java
@@ -1,14 +1,22 @@
 package mod.alexndr.netherrocks.init;
 
+import mod.alexndr.netherrocks.Netherrocks;
+import mod.alexndr.netherrocks.generation.IllumeniteBlobFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public final class ModFeatures
 {
 //    /** Feature<?> registry */
-//    public static final DeferredRegister<Feature<?>> FEATURES = 
-//    		DeferredRegister.create(ForgeRegistries.Keys.FEATURES, Netherrocks.MODID);
-//
-//    /** illumenite blob feature */
-//    public static final RegistryObject<Feature<NoneFeatureConfiguration>> ILLUMENITE_FEATURE = 
-//            FEATURES.register("illumenite_blob", () -> new IllumeniteBlobFeature(NoneFeatureConfiguration.CODEC));
+    public static final DeferredRegister<Feature<?>> FEATURES = 
+    		DeferredRegister.create(ForgeRegistries.Keys.FEATURES, Netherrocks.MODID);
+
+    /** illumenite blob feature */
+    public static final RegistryObject<Feature<NoneFeatureConfiguration>> ILLUMENITE_BLOB = 
+            FEATURES.register("illumenite_blob", () -> new IllumeniteBlobFeature(NoneFeatureConfiguration.CODEC));
 //
 //    /** ConfiguredFeature<?, ?> registry */
 //    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
