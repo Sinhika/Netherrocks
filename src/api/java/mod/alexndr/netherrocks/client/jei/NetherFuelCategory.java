42b5dcd1c1d7f05e2eac06922b087e2f6626cb14
Sinhika <sinhika@republicofnewhome.org>
2023-08-02 09:53:00 -0500

    updated JEI compatibility code to 1.20.1

--- src/api/java/mod/alexndr/netherrocks/client/jei/NetherFuelCategory.java
+++ src/api/java/mod/alexndr/netherrocks/client/jei/NetherFuelCategory.java
@@ -1,7 +1,5 @@
 package mod.alexndr.netherrocks.client.jei;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
@@ -12,6 +10,8 @@ import mezz.jei.api.recipe.RecipeType;
 import mod.alexndr.netherrocks.Netherrocks;
 import mod.alexndr.simplecorelib.api.client.jei.VeryAbstractFurnaceVariantCategory;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -40,15 +40,22 @@ public class NetherFuelCategory extends VeryAbstractFurnaceVariantCategory<Nethe
 	    builder.addSlot(RecipeIngredientRole.INPUT, 1, 17).addItemStacks(recipe.getInputs());
     }
 
-
 	@Override
-	public void draw(NetherFurnaceFuelRecipe recipe,  IRecipeSlotsView recipeSlotsView, PoseStack poseStack, double mouseX, double mouseY) 
+	public void draw(NetherFurnaceFuelRecipe recipe,  IRecipeSlotsView recipeSlotsView,  GuiGraphics guiGraphics, double mouseX, double mouseY) 
 	{
 		IDrawableAnimated flame = this.getFlame();
-		flame.draw(poseStack, 1, 0);
-		Minecraft minecraft = Minecraft.getInstance();
-		this.smeltCountText = createSmeltCountText(recipe.getBurnTime());
-		minecraft.font.draw(poseStack, smeltCountText, 24, 13, 0xFF808080);
+		flame.draw(guiGraphics, 1, 0);
+		drawCookTime(recipe, guiGraphics, 24, 13);
 	}
+
+	protected void drawCookTime(NetherFurnaceFuelRecipe recipe, GuiGraphics guiGraphics, int x, int y) 
+	{
+		this.smeltCountText = createSmeltCountText(recipe.getBurnTime());
+		Minecraft minecraft = Minecraft.getInstance();
+		Font fontRenderer = minecraft.font;
+		int stringWidth = fontRenderer.width(smeltCountText);
+		guiGraphics.drawString(fontRenderer, smeltCountText, getWidth() - stringWidth, y, 0xFF808080, false);
+	}
+
 
 } // end class
